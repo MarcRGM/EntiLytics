36db9b826aad4ddc87040652a2e721ce120020b1
MarcRGM <marcrodniemanalo@gmail.com>
2026-01-31 09:38:17 +0800

    WIP: Basic article and entity display

--- ui/ner_rss.py
+++ ui/ner_rss.py
@@ -5,6 +5,7 @@ from features.rss_handler import fetch_rss_articles
 text = solara.reactive("")
 continuous_update = solara.reactive(True)
 results = solara.reactive([])
+is_loading = solara.reactive(False) 
 
 def getArticles(rss_url):
     fetched_articles = fetch_rss_articles(rss_url)
@@ -12,6 +13,7 @@ def getArticles(rss_url):
     # Avoid flicker problems from using reactive variable
     temp_results = []
 
+    # Using [:3] for quick testing
     for article in fetched_articles[:3]:
         temp_results.append({
             "title" : article['title'],
@@ -19,23 +21,45 @@ def getArticles(rss_url):
             "entities" : identify_entities(article['description'])
         })
     results.set(temp_results)
-
-    for result in results.value:
-        print(f"Title: {result['title']}")
-        print(f"Description: {result['description']}")
-        print("Entities: ")
-        for ent in result['entities']:
-            print(f"{ent['text']}, {ent['label']}")
+    is_loading.set(False)
 
 def reset():
     text.set("")
+    results.set([]) # Clear the cards
+    is_loading.set(False)
+
+
+# UI Components
+@solara.component
+def ArticleListings():
+    # This component only renders if results.value is not empty
+    if results.value:
+        for i, article in enumerate(results.value, 1):
+            with solara.Card(
+                style={
+                    "padding": "3rem",
+                    "gap": "1rem",
+                    "background-color": "#34699A",
+                },
+                title=f"{i}. {article['title']}", 
+                elevation=5
+            ):
+                solara.Markdown(f"Description: {article['description'][:300]}...")
+                solara.Markdown(f"Entities:")
+                # Nested loop for entities
+                with solara.Column(
+                    style={
+                        "gap": "1rem",
+                        "background-color": "#34699A",
+                    }
+                ):
+                    for ent in article['entities']:
+                        solara.Markdown(f"Entity: {ent['text']},        Label: {ent['label']},      CF: {ent['confidence']}")
 
 
 @solara.component
 def Page():
     solara.lab.theme.dark = True
-    solara.Style("""
-    """)
     with solara.Column(
         style={
             "min-height": "100vh",
@@ -56,6 +80,7 @@ def Page():
             </div>
             """
         )
+
         with solara.Card(
                 style={
                     "padding": "3rem",
@@ -70,3 +95,10 @@ def Page():
                     solara.Button("Enter", on_click=lambda: getArticles(text.value), color="#75B06F",)
                     solara.Button("Clear", on_click=lambda: reset(), color="#CD5656",)
                 
+        # Show loading status if active
+        if is_loading.value:
+            solara.ProgressLinear() # horizontal progress bar
+            solara.Info("Fetching and analyzing articles...")
+
+        # Display the articles
+        ArticleListings()
